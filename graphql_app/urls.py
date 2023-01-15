b4c0d6f0af389a0c7fa07735f4c73afd1e11a62b
buguno <brunnodesouzabezerra@gmail.com>
2023-01-15 16:23:56 -0300

    refactor: adding graphql schema in urls

--- graphql_app/urls.py
+++ graphql_app/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView  # View for the user interface
+
+from graphql_app.schema import schema  # Schema we want to query
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # This URL will provide a user interface that is used to query the database
+    # and interact with the GraphQL API.
+    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
