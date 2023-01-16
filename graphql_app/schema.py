9062e7d65b4f7598601a931776dd5fb4c0f537e1
buguno <brunnodesouzabezerra@gmail.com>
2023-01-15 21:24:58 -0300

    refactor: separating create and update contact

--- graphql_app/schema.py
+++ graphql_app/schema.py
@@ -27,9 +27,27 @@ class Query(graphene.ObjectType):
         return Contact.objects.get(id=id)
 
 
-class ContactMutation(graphene.Mutation):
+class CreateContactMutation(graphene.Mutation):
     class Arguments:
         # Add fields you would like to create. This will corelate with the ContactType fields above.
+        name = graphene.String()
+        phone_number = graphene.String()
+
+    # Define the class we are getting the fields from
+    contact = graphene.Field(ContactType)
+
+    @classmethod
+    def mutate(cls, root, info, name, phone_number):
+        # Function that will save the data
+        contact = Contact(name=name, phone_number=phone_number)  # Accepts all fields
+        contact.save()
+
+        return CreateContactMutation(contact=contact)
+
+
+class UpdateContactMutation(graphene.Mutation):
+    class Arguments:
+        # Add fields you would like to update. This will corelate with the ContactType fields above.
         id = graphene.ID()
         name = graphene.String()
         phone_number = graphene.String()
@@ -40,20 +58,31 @@ class ContactMutation(graphene.Mutation):
     @classmethod
     def mutate(cls, root, info, id, name, phone_number):
         # Function that will save the data
-        contact = Contact(name=name, phone_number=phone_number)  # Accepts all fields
-        contact.save()
-
         get_contact = Contact.objects.get(id=id)
         get_contact.name = name
         get_contact.phone_number = phone_number
         get_contact.save()
-        return ContactMutation(contact=get_contact)
+
+        return UpdateContactMutation(contact=get_contact)
+
+
+class ContactDelete(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    contact = graphene.Field(ContactType)
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        contact = Contact(id=id)
+        contact.delete()
 
 
 class Mutation(graphene.ObjectType):
     # Keywords that will be used to do the mutation in the frontend
-    create_contact = ContactMutation.Field()
-    update_contact = ContactMutation.Field()
+    create_contact = CreateContactMutation.Field()
+    update_contact = UpdateContactMutation.Field()
+    delete_contact = ContactDelete.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
