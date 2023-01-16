f85fa9f6fd87caffd83358266dafb5445a589edf
buguno <brunnodesouzabezerra@gmail.com>
2023-01-15 21:04:24 -0300

    feat: adding update mutation

--- graphql_app/schema.py
+++ graphql_app/schema.py
@@ -30,24 +30,30 @@ class Query(graphene.ObjectType):
 class ContactMutation(graphene.Mutation):
     class Arguments:
         # Add fields you would like to create. This will corelate with the ContactType fields above.
+        id = graphene.ID()
         name = graphene.String()
         phone_number = graphene.String()
 
-    contact = graphene.Field(
-        ContactType
-    )  # Define the class we are getting the fields from
+    # Define the class we are getting the fields from
+    contact = graphene.Field(ContactType)
 
     @classmethod
-    def mutate(cls, root, info, name, phone_number):
+    def mutate(cls, root, info, id, name, phone_number):
         # Function that will save the data
         contact = Contact(name=name, phone_number=phone_number)  # Accepts all fields
         contact.save()
-        return ContactMutation(contact=contact)
+
+        get_contact = Contact.objects.get(id=id)
+        get_contact.name = name
+        get_contact.phone_number = phone_number
+        get_contact.save()
+        return ContactMutation(contact=get_contact)
 
 
 class Mutation(graphene.ObjectType):
     # Keywords that will be used to do the mutation in the frontend
     create_contact = ContactMutation.Field()
+    update_contact = ContactMutation.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
